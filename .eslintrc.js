404f3834f6695f847db8a0087655a6fd6e563733
priyabrata singh <priyabratasingh2005@gmail.com>
2025-04-05 15:25:43 +0530

    Remove unused files and configurations; delete .env, .eslintrc.js, .gitignore, package.json, and package-lock.json; add new assets including favicon and font files; create a new index.html and style.css for the project.

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['react-app', 'react-app/jest'],
-  rules: {
-    'no-unused-vars': 'warn', // Downgrade from error to warning
-    'react-hooks/exhaustive-deps': 'warn', // Ensure this is a warning
-  }
-}; 
